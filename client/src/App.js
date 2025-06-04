876750864333ffa49e70a8e2dad7d4c2368a485d
Coding Ranjith <ranjith.c96me@gmail.com>
2025-06-04 13:13:17 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// File: client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
@@ -6,20 +5,19 @@ import Register from './components/Register';
 import Dashboard from './pages/Dashboard';
 import Attendance from './components/Attendance';
 import EditUser from './components/EditUser';
-import { AuthProvider } from './components/AuthContext';
+// import { AuthProvider } from './components/AuthContext';
+
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        {/* Add your navigation bar or sidebar here if needed */}
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/attendance" element={<Attendance />} />
-        <Route path="/edit-user" element={<EditUser />} />
-      </Routes>
-      </AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} /> {/* Changed default route */}
+          <Route path="/login" element={<Login />} /> {/* Optional if you still want access */}
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/edit-user" element={<EditUser />} />
+        </Routes>
     </Router>
   );
 }
