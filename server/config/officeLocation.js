03d5e4d9421ad251dd72f7b8c0b00c2fd62ff40f
Coding Ranjith <ranjith.c96me@gmail.com>
2025-07-02 13:08:31 +0530

    update

--- server/config/officeLocation.js
+++ server/config/officeLocation.js
@@ -3,12 +3,12 @@ module.exports = [
     name: "Office 1",
     latitude: 12.94198577,
     longitude: 80.21012198,
-    radiusMeters: 200, // Acceptable distance from Office 1
+    radiusMeters: 200,
   },
   {
     name: "Office 2",
     latitude: 13.0023424,
     longitude: 80.2193408,
-    radiusMeters: 300, // Acceptable distance from Office 2
+    radiusMeters: 300,
   }
 ];
